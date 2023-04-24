09535af0beaffbecca233cd37b634778a34fa8c0
Marco <10555523+mrcogllrdo@users.noreply.github.com>
2023-04-23 20:38:02 -0700

    Removed extraneous sections of pullData.py
    Forgot to remove the sections of code from an entirely unrelated script. Repurposed a copy of an old script as a template; snippets are still in pullDataOrig.py, will remove later once I get the script fully working.

--- pullData.py
+++ pullData.py
@@ -86,66 +86,6 @@ def determineFileName():
     logFileName = prefixFileName + "log"
     return (geoFileName, logFileName)
 
-def groupByCategory(jsonData, jsonSchema, BMITable):
-    """
-        Reads in BMI table & BMI data & data schema, validates data, calculates
-        each sample's BMI (if applicable), appends 3 new columns with relevant
-        info to each valid sample, then counts which of them pass our specified
-        desired category filter ('Overweight' by default)
-
-        Args:
-          jsonData - Array of dicts representing individuals
-          jsonSchema - JSON dict to validate individuals' info is right format
-          BMITable - JSON dict to capture BMI categories and health risk
-
-        Returns: Tuple of grouped data and number of invalid samples
-    """
-    # Iterate through given JSON Data
-    numberInvalid = 0
-    groupedData = {key:[] for key in BMITable} # JSON data grouped by category
-    for sample in jsonData:
-        try:
-            # Validate data
-            validateJSONSchema(sample, jsonSchema)
-
-            # Data is valid so calculate BMI
-            height = sample['HeightCm'] / 100
-            mass = sample['WeightKg']
-            sampleBMI = calculateBMI(mass, height)
-
-            # Determine BMI category the sample belongs to
-            matchedCategory = None
-            for BMICategory in BMITable: # Check all categories
-                BMILowerLimit = BMITable[BMICategory]['BMILowerLimit']
-                BMIUpperLimit = BMITable[BMICategory]['BMIUpperLimit']
-                if BMIUpperLimit == 'inf' and sampleBMI >= BMILowerLimit:
-                    matchedCategory = BMICategory
-                    break
-                elif sampleBMI >= BMILowerLimit and sampleBMI <= BMIUpperLimit:
-                    matchedCategory = BMICategory
-                    break
-
-            # Then add sample if a matched category was found
-            if matchedCategory:
-                healthRisk = BMITable[BMICategory]['healthRisk']
-                newInfo = (
-                    {
-                        'BMI': sampleBMI,
-                        'BMICategory': matchedCategory,
-                        'HealthRisk': healthRisk
-                    }
-                )
-                updatedSample = sample | newInfo # Append 3 new columns
-                groupedData[matchedCategory].append(updatedSample)
-            else:
-                raise IOError() # No BMI table match, skip sample
-
-        except Exception: # Schema mismatch, BMI invalid, or no BMI Table match
-            numberInvalid += 1
-
-    # Return stats
-    return (groupedData, numberInvalid)
-
 def setupLogging(logFile):
     """
         Sets up logging with an initial logging message to indicate logger has
@@ -165,39 +105,6 @@ def parseJSONData(jsonData):
     """
     """
     pass
-
-def printResults(groupedData, filterCategory='Overweight'):
-    """
-        Displays number of people in specified category, 'Overweight' by default
-
-        Args:
-          groupedData - Dict of BMI categories, each category being an array of
-          dicts where each dict represents an individual's information
-          filterCategory - Key to search for in groupedData, 'Overweight' by
-          default
-
-        Returns: Integer representing number of people in the specified BMI
-        category
-    """
-    matched = len(groupedData.get(filterCategory, {}))
-    statsMessage = (
-        f'Script has finished!\nThe number of people that match the '
-        f'BMI category \'{filterCategory}\' is {matched}'
-    )
-    print(statsMessage)
-
-def readJSON(fileLocation=None):
-    """
-        Tries to read in a JSON file and stores data.
-
-        Args:
-          String denoting file location of JSON data
-
-        Returns: List of Dict or Dict, depending on JSON data
-    """
-    with open(fileLocation) as jsonFile:
-        data = json.load(jsonFile)
-        return data
 
 def requestEONETData(tryTimes = 3, pauseTime = 3, getCategories = False, 
                      getSources = False):
@@ -259,70 +166,7 @@ def requestHTTPJSON(URL, tryTimes, pauseTime):
     if not success: # Tried tryTimes yet still failed; can't run rest of script
         response.raise_for_status()
     return response.json()
-    
-def scriptParse():
-    """
-        Tries to parse args passed to script
-
-        Returns: Object with script's arguments as attribs of its namespace
-    """
-    # Meta info
-    scriptDescription = (
-        f'Calculates folks\' BMI from JSON data then displays how many of them '
-        f'are considered to be a part of the specified BMI category, '
-        f'\'Overweight\' by default'
-    )
-    fileLocationHelp = f'File location of JSON data relative to script location'
-    schemaLocationHelp = (
-        f'Schema location relative to script location for validating JSON input'
-        f' data'
-    )
-    tableLocationHelp = (
-        f'Table location relative to script location for assigning BMI category'
-        f' and health risk to BMI Range'
-    )
-
-    # Parse args
-    try:
-        parser = argparse.ArgumentParser(scriptDescription)
-        parser.add_argument('--fileLocation', type=str, help=fileLocationHelp)
-        parser.add_argument('--schemaLocation', type=str, help=schemaLocationHelp)
-        parser.add_argument('--tableLocation', type=str, help=tableLocationHelp)
-        args = parser.parse_args()
-        return args
-    except Exception:
-        raise IOError("Error reading script arguments, refer to --help")
-        
-def validateJSONSchema(sample, jsonSchema):
-    """
-        Validates given JSON data according to specified schema
-
-        Args:
-          sample - JSON dict to validate
-          jsonSchema - JSON dict to validate sample against
-
-        Returns: True if valid, else raise exception
-    """
-    validate(instance=sample, schema=jsonSchema)
-    return True
-
-def writeJSON(rawData, fileLocation='.'):
-    """
-        Serializes data then writes JSON to a text file, saved in the same
-        location as the script by default.
-
-        Args:
-          rawData - Raw data to serialize then store
-
-        Returns: None as text file is created
-    """
-    # Serialize data to JSON
-    jsonData = json.dumps(rawData, indent=4)
-
-    # Write JSON data to text file
-    with open(f"{fileLocation}/categorizedData.txt", "w") as outFile:
-        outFile.write(jsonData)
-
+           
 ## MAIN
 if __name__ == "__main__":
 
@@ -347,22 +191,6 @@ if __name__ == "__main__":
         
         # Display number of events and disasters parsed + if any were skipped
         pass
-        
-        ## OLD STUFF
-
-        # # Read in table of BMI Category and Health Risk
-        # BMITable = readJSON(args.tableLocation)
-
-        # # Validate data matches schema then group data
-        # jsonSchema = readJSON(args.schemaLocation)
-        # groupedData, invalidCount = groupByCategory(jsonData, jsonSchema, BMITable)
-        # print(f'\nFound {invalidCount} invalid BMI samples! Skipping those\n')
-
-        # # Write valid, grouped data to a new JSON file in script's location
-        # writeJSON(groupedData)
-
-        # # Display number of people in a specified category, 'Overweight' default
-        # printResults(groupedData)
 
     except Exception as error:
         logging.exception("Traceback of error:")
